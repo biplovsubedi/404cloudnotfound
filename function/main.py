2dfc26fd513ed2bb0aba88894b03adee86ae7de3
biplov-subedi <biplovsubedi@gmail.com>
2019-07-03 20:24:31 +0530

    key update

--- function/main.py
+++ function/main.py
@@ -3,6 +3,8 @@ import json
 import boto3
 import googlemaps
 
+from googleroutes.py import fetchDetails,timeConvert,gmapsQuery
+
 APIKEY = 'c031ff79d81c1f77c4331df2ac93ed4b'
 
 
@@ -31,6 +33,26 @@ def api_request(query):
 	res_dict = json.loads(res_str)
 	print("Type of res_dict", str(type(res_dict)))
 	return res_dict
+
+def weather_status(weather):
+    
+	
+    
+    weather_condition_good = [300, 301 , 302 , 310, 311, 312 , 313 , 314 , 321 ,500 , 501, 520 , 521 , 522, 531, 601,800,801,802,803,804]
+    weather_conition_bad  = [502, 503 ,611, 612,613,615,616,620,621 ,701,711,721,731,741,751,761,762,771 ]
+    weather_condition_severe  = [504, 511,602,622, 781, 200 , 201 , 202 , 211 , 212 , 210 , 221, 231 , 232, 230 ]
+
+    if ID in weather_condition_good:
+        
+        return "Good Morniing , today you'll see " +weather(ID)+" Have a wonderful day :) "
+
+    if ID in weather_condition_bad:
+        return "Good Morniing , today you'll see " +weather(ID)+" Have a wonderful day :) "
+
+    if ID in weather_condition_severe:
+        return "Good Morniing , today you'll see " +weather(ID)+" Please consider staying back at home  as weather conditions may get worse, Have a Wonderful Day :) "
+
+    
 
 def get_weather(latitude,longitude):
 	
@@ -108,7 +130,17 @@ def process_all_users():
 		home_loc = user['home_loc']['S']
 
 		##TODO --  process for each user
-		
+		resp = gmapsQuery(home_loc, office_loc, "driving",timeConvert(morning_departure))
+		all_results = fetchDetails(resp)
+
+		#Office Weather
+		office_weather = get_weather(all_results[4],all_results[5])
+
+		#Home Weather
+		home_weather = get_weather(all_results[2],all_results[3])
+
+
+
 
 
 def send_sns_sms(contact,message):
