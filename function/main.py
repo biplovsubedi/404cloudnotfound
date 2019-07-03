19301c9ee667a5b716234cd364937f2474e38484
biplov-subedi <biplovsubedi@gmail.com>
2019-07-03 20:32:14 +0530

    weather staus

--- function/main.py
+++ function/main.py
@@ -36,21 +36,22 @@ def api_request(query):
 
 def weather_status(weather):
     
-	
-    
+	w_id = int(weather['id'])
+	w_main = weather['main']
+	w_desc = weather['description']
+
     weather_condition_good = [300, 301 , 302 , 310, 311, 312 , 313 , 314 , 321 ,500 , 501, 520 , 521 , 522, 531, 601,800,801,802,803,804]
     weather_conition_bad  = [502, 503 ,611, 612,613,615,616,620,621 ,701,711,721,731,741,751,761,762,771 ]
     weather_condition_severe  = [504, 511,602,622, 781, 200 , 201 , 202 , 211 , 212 , 210 , 221, 231 , 232, 230 ]
 
-    if ID in weather_condition_good:
-        
-        return "Good Morniing , today you'll see " +weather(ID)+" Have a wonderful day :) "
+    if w_id in weather_condition_good:
+        return "Good Morniing {}, today you'll see " +w_desc+" Have a wonderful day :) "
 
-    if ID in weather_condition_bad:
-        return "Good Morniing , today you'll see " +weather(ID)+" Have a wonderful day :) "
+    if w_id in weather_condition_bad:
+        return "Good Morniing {}, today you'll see " +w_desc+" Have a wonderful day :) "
 
-    if ID in weather_condition_severe:
-        return "Good Morniing , today you'll see " +weather(ID)+" Please consider staying back at home  as weather conditions may get worse, Have a Wonderful Day :) "
+    if w_id in weather_condition_severe:
+        return "Good Morniing {}, today you'll see " +w_desc+" Please consider staying back at home  as weather conditions may get worse, Have a Wonderful Day :) "
 
     
 
@@ -139,6 +140,7 @@ def process_all_users():
 		#Home Weather
 		home_weather = get_weather(all_results[2],all_results[3])
 
+		home_weather_status = weather_status(home_weather)
 
 
 
